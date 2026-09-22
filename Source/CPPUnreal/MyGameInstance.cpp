626ca47d3d4e9c1a2b4000db24cd2c55743b96c0
newyoungbins <dudqlsjyb@naver.com>
2026-09-22 17:00:07 +0900

    재밌는 언리얼 assert와 check, ensure배움

--- Source/CPPUnreal/MyGameInstance.cpp
+++ Source/CPPUnreal/MyGameInstance.cpp
@@ -3,49 +3,33 @@
 
 #include "MyGameInstance.h"
 
+UMyGameInstance::UMyGameInstance()
+{
+	// 기본값 설정.
+	// 생성자에서 설정하는 기본 값은 CDO 템플릿 객체에 저장됨.
+	SchoolName = TEXT("기본 학교");
+}
+
 void UMyGameInstance::Init()
 {
 	Super::Init();
 
-	//UE_LOG(LogTemp, Log, TEXT("Hello Unreal"));
-	TCHAR LogCharArray[] = TEXT("Hello Unreal");
-	UE_LOG(LogTemp, Log, TEXT("%s"), LogCharArray);
+	UE_LOG(LogTemp, Log, TEXT("======================"));
 
-	FString LogCharString = LogCharArray;
-	UE_LOG(LogTemp, Log, TEXT("%s"), *LogCharString);
+	// 클래스 정보 가져오기.
+	UClass* ClassRuntime = GetClass();
+	UClass* ClassCompile = UMyGameInstance::StaticClass();
 
-	// 문자열 복사.
-	TCHAR LogCharArrayWithSize[100] = {};
-	FCString::Strncpy(LogCharArrayWithSize, *LogCharString, LogCharString.Len());
+	// 두 정보가 같은지 비교.
+	//check(ClassRuntime == ClassCompile);
+	//ensure(ClassRuntime == ClassCompile);
 
-	UE_LOG(LogTemp, Log, TEXT("%s"), LogCharArrayWithSize);
+	UE_LOG(LogTemp, Log, TEXT("학교를 담당하는 클래스 이름 : %s"), *ClassRuntime->GetName());
 
-	// 문자열 자르기.
-	if (LogCharString.Contains(TEXT("unreal"), ESearchCase::IgnoreCase))
-	{
-		// 문자열을 검색해 시작 인덱스 얻기.
-		int32 Index = LogCharString.Find(TEXT("unreal"), ESearchCase::IgnoreCase);
-		FString EndString = LogCharString.Mid(Index);
-		UE_LOG(LogTemp, Log, TEXT("%s"), *EndString);
-	}
+	SchoolName = TEXT("포텐업");
 
-	// 문자열 나누기.
-	FString Left, Right;
-	if (LogCharString.Split(TEXT(" "), &Left, &Right))
-	{
-		UE_LOG(LogTemp, Log, TEXT("Split Test: %s / %s"), *Left, *Right);
-	}
+	UE_LOG(LogTemp, Log, TEXT("학교 이름: %s"), *SchoolName);
+	UE_LOG(LogTemp, Log, TEXT("학교 이름 기본값: %s"), *GetClass()->GetDefaultObject<UMyGameInstance>()->SchoolName);
 
-	// 문자열 조합.
-	int32 IntValue = 40;
-	float FloatValue = 3.141592f;
-
-	FString FloatIntString = FString::Printf(TEXT("Int:%d, Float:%f"), IntValue, FloatValue);
-	UE_LOG(LogTemp, Log, TEXT("%s"), *FloatIntString);
-
-	// FName 특성 살펴보기 - 대소문자 구별하지 않음.
-	FName Key1(TEXT("PELVEIS"));
-	FName Key2(TEXT("PELVEIS"));
-
-	UE_LOG(LogTemp, Log, TEXT("FName 비교 결과: %s"), (Key1 == Key2 ? TEXT("같음") : TEXT("다름")));
+	UE_LOG(LogTemp, Log, TEXT("======================"));
 }
--- Source/CPPUnreal/MyGameInstance.h
+++ Source/CPPUnreal/MyGameInstance.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -14,5 +14,14 @@ class CPPUNREAL_API UMyGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
+public:
+	UMyGameInstance();
+
+private:
+	// 게임 인스턴스 초기화 함수.
 	virtual void Init() override;
+
+private:
+	UPROPERTY()
+	FString SchoolName;
 };
